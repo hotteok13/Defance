d6c1678c31318ebc5141a193a6fd2f027d04b9f3
Eripron <87156859+Eripron@users.noreply.github.com>
2022-06-22 21:51:32 +0900

    Defance

---
+++ Assets/Script/CreateMonster.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMonster : MonoBehaviour
+{
+    
+    public void GoblinCreate()
+    {
+        Instantiate(Resources.Load<GameObject>("Goblin_B"),new Vector3(0,0,13),Quaternion.Euler(0,0,0));
+    }
+    public void HapineCreate()
+    {
+        Instantiate(Resources.Load<GameObject>("Hapine"), new Vector3(0, 0, 13), Quaternion.Euler(0, 0, 0));
+    }
+}
